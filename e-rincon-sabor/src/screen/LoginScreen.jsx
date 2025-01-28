36ba3d4e800a2f452bad3c4d86c62abc252ff5f3
Danu Villa <daniela.lucia.villa@hotmail.com>
2025-01-28 13:03:12 -0300

    correciones y agregados en login, hecho screen registro

--- e-rincon-sabor/src/screen/LoginScreen.jsx
+++ e-rincon-sabor/src/screen/LoginScreen.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 import style from '../styles/loginRegister.module.css'
+import { Link } from 'react-router-dom';
+import logo from '../assets/iconNavBlack.png';
+import icon from '../assets/arrowBackLeft.png'
 
 export default function LoginScreen() {
     const [formData, setFormData] = useState({ name: "", password: "" });
@@ -16,6 +19,16 @@ export default function LoginScreen() {
     return (
         <div className={style["login-container"]}>
             <div className={style["login-box"]}>
+                <div className={style['logo-container']}>
+                    <div className={style["wrapper-icon"]}>
+                        <Link to="/">
+                            <img src={icon} alt="Categorias" className={style["icon"]} />
+                        </Link>
+                    </div>
+                    <div className={style['wrapper-logo']}>
+                        <img src={logo} alt="Categorias" className={style["logo"]} />
+                    </div>
+                </div>
                 <h2>LOGIN</h2>
                 <form onSubmit={handleSubmit}>
                     <div className={style["input-group"]}>
--- e-rincon-sabor/src/screen/RegisterScreen.jsx
+++ e-rincon-sabor/src/screen/RegisterScreen.jsx
@@ -1,8 +1,94 @@
-
+import { useState } from 'react';
+import style from '../styles/loginRegister.module.css'
+import { Link } from 'react-router-dom';
+import logo from '../assets/iconNavBlack.png';
+import icon from '../assets/arrowBackLeft.png'
 export default function RegisterScreen() {
+    const [formData, setFormData] = useState({
+        firstName: "",
+        lastName: "",
+        username: "",
+        email: "",
+        password: "",
+    });
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Registering with:", formData);
+    };
+
     return (
-        <div>
-            <h2>You are in the Register</h2>
+        <div className={style["register-container"]}>
+            <div className={style["register-box"]}>
+                <div className={style['logo-container']}>
+                    <div className={style["wrapper-icon"]}>
+                        <Link to="/">
+                            <img src={icon} alt="Categorias" className={style["icon"]} />
+                        </Link>
+                    </div>
+                    <div className={style['wrapper-logo']}>
+                        <img src={logo} alt="Categorias" className={style["logo"]} />
+                    </div>
+                </div>
+                <h2>Registro</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className={style["input-group"]}>
+                        <label>First Name</label>
+                        <input
+                            type="text"
+                            name="firstName"
+                            value={formData.firstName}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <div className={style["input-group"]}>
+                        <label>Last Name</label>
+                        <input
+                            type="text"
+                            name="lastName"
+                            value={formData.lastName}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <div className={style["input-group"]}>
+                        <label>Username</label>
+                        <input
+                            type="text"
+                            name="username"
+                            value={formData.username}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <div className={style["input-group"]}>
+                        <label>Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <div className={style["input-group"]}>
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <button className={style["btt-register"]} type="submit">✅ Register</button>
+                </form>
+            </div>
         </div>
-    )
+    );
 }
